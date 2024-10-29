3c938bf622621e722e79abb7ebe2ba27af721ced
Oli Legat <olivier.legat@ag-grid.com>
2024-10-29 12:20:54 +0000

    AG-13291 Mimic ag-grid tab guard as closely as possible
    The tabGuards...:
    
    -   Have their tabindex values set to the keyboard.tabIndex user option.
    
    -   Surround the entire chart (before & after '.ag-charts-canvas-center').
    
    -   Remove the 'tabindex' when the chart is blurred, and restore the 'tabindex'
        values when the chart is focused.

--- packages/ag-charts-community/src/util/guardedElement.ts
+++ packages/ag-charts-community/src/util/guardedElement.ts
@@ -1,44 +1,26 @@
-export type GuardedElementProperties = {
-    element: HTMLElement;
-    topTabGuard: HTMLElement;
-    bottomTabGuard: HTMLElement;
-};
+import { setAttribute } from './attributeUtil';
 
-export class GuardedElement<TElement extends HTMLElement = HTMLElement> implements GuardedElementProperties {
+export class GuardedElement {
     private readonly destroyFns: (() => void)[] = [];
-
-    private guardTarget?: HTMLElement;
     private guardTabIndex: number = 0;
-
-    private guessedDelta?: -1 | 0 | 1;
+    private hasFocus = false;
 
     constructor(
-        public readonly element: TElement,
-        public readonly topTabGuard: HTMLElement,
-        public readonly bottomTabGuard: HTMLElement
+        private readonly element: HTMLElement,
+        private readonly topTabGuard: HTMLElement,
+        private readonly bottomTabGuard: HTMLElement
     ) {
-        this.element.tabIndex = -1;
-        this.initEventListener(this.element, 'blur', () => this.onBlur());
-        this.initEventListener(this.element, 'focus', () => this.onFocus());
-        this.initEventListener(this.topTabGuard, 'focus', (ev) => this.onTabStart(ev, this.topTabGuard));
-        this.initEventListener(this.bottomTabGuard, 'focus', (ev) => this.onTabStart(ev, this.bottomTabGuard));
+        this.initTabGuard(this.topTabGuard, () => this.onTab(false));
+        this.initTabGuard(this.bottomTabGuard, () => this.onTab(true));
+        this.element.addEventListener('focus', () => this.onFocus(), { capture: true });
+        this.element.addEventListener('blur', () => this.onBlur(), { capture: true });
     }
 
     set tabIndex(index: number) {
-        if (index >= 0) {
-            // Save this index so that this.element can restore it when it goes out of focus.
-            this.guardTabIndex = index;
-        }
-
-        // Strictly positive tab-indices breaks our ability to tab guards to detect which direction the focus
-        // is coming from. So if it is the value that the user has specified, then disable the bottom tab-guard
-        // to treat TAB and Shift+TAB the same way.
-        if (index > 0) {
-            this.topTabGuard.tabIndex = index;
-            this.bottomTabGuard.style.display = 'none';
-        } else {
-            this.topTabGuard.tabIndex = index;
-            this.bottomTabGuard.tabIndex = index;
+        this.guardTabIndex = index;
+        if (!this.hasFocus) {
+            this.topTabGuard.tabIndex = this.guardTabIndex;
+            this.bottomTabGuard.tabIndex = this.guardTabIndex;
         }
     }
 
@@ -47,51 +29,33 @@ export class GuardedElement<TElement extends HTMLElement = HTMLElement> implemen
         this.destroyFns.length = 0;
     }
 
-    private initEventListener(
-        elem: HTMLElement,
-        type: 'focus' | 'blur',
-        handler: (() => void) | ((e: FocusEvent) => void)
-    ) {
-        elem.addEventListener(type, handler);
-        this.destroyFns.push(() => elem.removeEventListener(type, handler));
-    }
-
-    private onBlur() {
-        if (this.element.tabIndex === -1) {
-            this.tabIndex = this.guardTabIndex;
-            this.guardTarget = undefined;
-            this.guessedDelta = undefined;
-        }
+    private initTabGuard(guard: HTMLElement, handler: () => void) {
+        guard.addEventListener('focus', handler);
+        this.destroyFns.push(() => guard.removeEventListener('focus', handler));
     }
 
     private onFocus() {
-        if (this.element.tabIndex === -1) {
-            this.tabIndex = -1;
-        }
+        this.hasFocus = true;
+        setAttribute(this.topTabGuard, 'tabindex', undefined);
+        setAttribute(this.bottomTabGuard, 'tabindex', undefined);
     }
 
-    private onTabStart(event: FocusEvent, target: HTMLElement) {
-        // If the userOptions specified `keyboard.tabIndex > 0`, then we can still try to guess the delta
-        // by comparing event.relatedTarget.tabIndex. This isn't guaranteed to work though, so fall back
-        // to delta = 0 if something isn't right.
-        if (target.tabIndex > 0) {
-            this.guessedDelta = 0; // fallback
-            if (event.relatedTarget != null && 'tabIndex' in event.relatedTarget) {
-                const prevTabIndex = Number(event.relatedTarget['tabIndex']);
-                if (!isNaN(prevTabIndex) && prevTabIndex > 0) {
-                    this.guessedDelta = prevTabIndex < target.tabIndex ? 1 : -1;
-                }
-            }
-        }
-        this.guardTarget = target;
-        this.element.focus();
+    private onBlur() {
+        this.hasFocus = false;
+        this.topTabGuard.tabIndex = this.guardTabIndex;
+        this.bottomTabGuard.tabIndex = this.guardTabIndex;
     }
 
-    public getBrowserFocusDelta(): -1 | 0 | 1 {
-        const { guessedDelta, guardTarget, topTabGuard, bottomTabGuard: botTabGuard } = this;
-        if (guessedDelta !== undefined) return guessedDelta;
-        if (guardTarget === topTabGuard) return 1;
-        if (guardTarget === botTabGuard) return -1;
-        return 0;
+    private onTab(reverse: boolean) {
+        this.getFocusableTarget(reverse)?.focus?.();
+    }
+
+    private getFocusableTarget(reverse: boolean): (Element & { focus?: () => void }) | undefined {
+        if (reverse) {
+            const focusables = this.element.querySelectorAll('[tabindex="0"]');
+            return focusables.item(focusables.length - 1);
+        } else {
+            return this.element.querySelector('[tabindex="0"]') ?? undefined;
+        }
     }
 }
