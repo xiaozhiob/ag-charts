93094b7c4a19b0979c9f1f400a94416ac2b14b72
Alan Treadway <alan.treadway@ag-grid.com>
2024-11-01 11:10:33 +0000

    AG-13304 - Allow Chart.waitForUpdate() to finish in <50ms.

--- packages/ag-charts-community/src/util/async.ts
+++ packages/ag-charts-community/src/util/async.ts
@@ -3,3 +3,29 @@ export function sleep(sleepTimeoutMs: number): Promise<void> {
         setTimeout(() => resolve(undefined), sleepTimeoutMs);
     });
 }
+
+export class AsyncAwaitQueue {
+    private readonly queue: (() => void)[] = [];
+
+    public await(timeout = 50) {
+        return new Promise<boolean>((resolve) => {
+            const successFn = () => {
+                clearTimeout(timeoutHandle);
+                resolve(true);
+            };
+            const timeoutFn = () => {
+                const queueIndex = this.queue.findIndex(successFn);
+                if (queueIndex < 0) return;
+
+                this.queue.splice(this.queue.findIndex(successFn), 1);
+                resolve(false);
+            };
+            const timeoutHandle = setTimeout(timeoutFn, timeout);
+            this.queue.push(successFn);
+        });
+    }
+
+    public notify() {
+        this.queue.splice(0).forEach((cb) => cb());
+    }
+}
