68850ae9005a2d31d8994758e6b99c0ddcaa9f24
iMoses <imoses.g@gmail.com>
2024-11-07 13:14:35 +0000

    Add types for grid integration (#2933)
    * Add types for grid integration
    
    * fix lint
    
    * fix build
    
    * fix build + cr notes
    
    * revert
    
    * cr changes

--- packages/ag-charts-community/src/main-modules.ts
+++ packages/ag-charts-community/src/main-modules.ts
@@ -1,6 +1,34 @@
-export { AgCharts } from './api/agCharts';
-export { VERSION } from './version';
-export * as _Scene from './integrated-charts-scene';
-export * as _Theme from './integrated-charts-theme';
-export * as _Util from './integrated-charts-util';
-export { registerInbuiltModules as setupCommunityModules } from './chart/factory/registerInbuiltModules';
+import type { IntegratedChartModule, IntegratedSparklineModule } from 'ag-charts-types';
+
+import { AgCharts } from './api/agCharts';
+import { registerInbuiltModules } from './chart/factory/registerInbuiltModules';
+import * as _Scene from './integrated-charts-scene';
+import * as _Theme from './integrated-charts-theme';
+import * as _Util from './integrated-charts-util';
+import { VERSION } from './version';
+
+export const ChartCommunityModule: IntegratedChartModule = {
+    AgCharts,
+    VERSION,
+    // @ts-ignore
+    _Scene,
+    // @ts-ignore
+    _Theme,
+    _Util,
+    create: AgCharts.create,
+    setup: registerInbuiltModules,
+    isEnterprise: false,
+};
+
+export const SparklineModule: IntegratedSparklineModule = {
+    AgCharts,
+    VERSION,
+    // @ts-ignore
+    _Scene,
+    // @ts-ignore
+    _Theme,
+    _Util,
+    create: AgCharts.__createSparkline,
+    setup: registerInbuiltModules,
+    isEnterprise: false,
+};
